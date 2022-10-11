8f90663657621c88845ee2db09a0515168fd43a8
Yash Gupta <itsyashgupta.18@gmail.com>
2022-10-12 00:13:30 +0530

    integer issue - update 1

--- config/database.php
+++ config/database.php
@@ -89,6 +89,10 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'options' => array(
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+                PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE => true
+            )
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
