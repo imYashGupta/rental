aba6681fc039006c6db3528bf98b62504e0eff04
Yash Gupta <itsyashgupta.18@gmail.com>
2022-11-07 19:05:37 +0530

    remaining rent added

---
+++ public/build/assets/Checkbox.2cb5a398.js
@@ -0,0 +1,1 @@
+import{a as s}from"./app.0b059cf6.js";function a({name:o,value:r,handleChange:i,checked:n}){return s("input",{type:"checkbox",name:o,value:r,className:"rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50",onChange:e=>i(e),checked:n})}export{a as C};
